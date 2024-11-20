6763d74ee8b07315e14807639a361ce2aa548eba
vigneshwar kandhaiya <vigneshwar.k2@gmail.com>
2024-11-21 00:41:59 +0530

    Model trainer module completed

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import os
 import sys
 from src.networksecurity.logger.logger import logger
 from src.networksecurity.exception.exception import NetworkSecurityException
-from src.networksecurity.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from src.networksecurity.entity.config_entity import (TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,
+                                                      DataTransformationConfig,ModelTrainerConfig)
 from src.networksecurity.components.data_ingestion import DataIngestion
 from src.networksecurity.components.data_validation import DataValidation
 from src.networksecurity.components.data_transformation import DataTransformation
+from src.networksecurity.components.model_trainer import ModelTrainer
 
 if __name__=="__main__":
     logger.info("STAGE <<<<<<<<<<<< Initializing Training Pipeline Config >>>>>>>>>>>>")
     training_pipeline_config=TrainingPipelineConfig()
     logger.info("STAGE <<<<<<<<<<<< Initiatized Pipeline Config Sucessessfully >>>>>>>>>>>>")
+
+    ################################## Data Ingestion ##################################
+
 
     logger.info("STAGE <<<<<<<<<<<< Initializing the Dataingestion Config >>>>>>>>>>>>")
     data_ingestion_config=DataIngestionConfig(training_pipeline_config)
@@ -22,6 +27,8 @@ if __name__=="__main__":
     logger.info("STAGE <<<<<<<<<<<< Initiating the data Ingestion and archiving artifacts >>>>>>>>>>>>")
     data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
     logger.info("STAGE <<<<<<<<<<<< Data Ingestion Completed >>>>>>>>>>>>")
+
+    ################################## Data Validation ##################################
 
     logger.info("STAGE <<<<<<<<<<<< Initializing the Data Validation Config >>>>>>>>>>>>")
     data_validation_config=DataValidationConfig(training_pipeline_config)
@@ -48,4 +55,20 @@ if __name__=="__main__":
     logger.info("STAGE <<<<<<<<<<<< Initiating the data transformation and collecting transformed data and object >>>>>>>>>>>>")
     data_transformation_artifact=data_transformation.initiate_data_transformation()
     print(data_transformation_artifact)
+    logger.info("STAGE <<<<<<<<<<<< Data Transformation Completed >>>>>>>>>>>>")
+
+
+    ################################## Model Trainer ##################################
+
+    logger.info("STAGE <<<<<<<<<<<< Initializing the Model Config >>>>>>>>>>>>")
+    model_trainer_config=ModelTrainerConfig(training_pipeline_config)
+    logger.info("STAGE <<<<<<<<<<<< Initialized the Model Trainer Config Successfully >>>>>>>>>>>>")
+
+    logger.info("STAGE <<<<<<<<<<<< Initiating Data Transformation Pipeline >>>>>>>>>>>>")
+    model_trainer=ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                           model_trainer_config=model_trainer_config)
+    
+    logger.info("STAGE <<<<<<<<<<<< Initiating the data transformation and collecting transformed data and object >>>>>>>>>>>>")
+    model_trainer_artifact=model_trainer.initiate_model_training()
+    print(model_trainer_artifact)
     logger.info("STAGE <<<<<<<<<<<< Data Transformation Completed >>>>>>>>>>>>")
--- src/networksecurity/utils/utils.py
+++ src/networksecurity/utils/utils.py
@@ -3,6 +3,8 @@ import sys
 import yaml
 import numpy as np
 import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score
 from src.networksecurity.logger.logger import logger
 from src.networksecurity.exception.exception import NetworkSecurityException
 
@@ -51,4 +53,51 @@ def save_object(file_path:str,obj:object) -> None:
         with open(file_path,"wb") as file_obj:
             pickle.dump(obj,file_obj)
     except Exception as e:
-        raise NetworkSecurityException
+        raise NetworkSecurityException(e,sys)
+
+def load_object(file_path):
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} is not exists")
+        with open(file_path,"rb") as obj:
+            return pickle.load(obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+def load_numpy_data_array(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) 
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = f1_score(y_train, y_train_pred)
+
+            test_model_score = f1_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
