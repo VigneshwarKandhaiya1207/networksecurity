77c4e7b05adbcc64eab8f68bc5c59b54bdb2d501
vigneshwar kandhaiya <vigneshwar.k2@gmail.com>
2024-11-22 18:16:41 +0530

    modified s3 pusher add the logger

--- src/networksecurity/components/s3_artifacts_model_pusher.py
+++ src/networksecurity/components/s3_artifacts_model_pusher.py
@@ -3,12 +3,14 @@ import sys
 import boto3
 import subprocess
 from dotenv import load_dotenv
+from src.networksecurity.logger.logger import logger
 
 load_dotenv()
 
 class S3Client:
 
     def push_to_s3(self,local_directory,bucket_path):
+        logger.info("Pushing the artifacts to the S3 bucket")
         command_to_run=f"aws s3 sync {local_directory} {bucket_path}"
         subprocess.run(command_to_run,shell=True)
         
